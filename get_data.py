7fa589a6672e7c0bdece62abd1e37ec79a8ea648
alvations <alvations@gmail.com>
2015-08-03 12:17:57 +0200

    added some concurrency for downloading parallel and monolingual data

--- get_data.py
+++ get_data.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python -*- coding: utf-8 -*-
 
 import os
+from multiprocessing import Process
 
 import wmt_data 
-from utils import parallelized_commandline
+from utils import parallelized_download
 
 def download_wmt_monolingual(lang, maxp=5):
     langdir = 'data/mono/' + lang + '/'
     os.makedirs(langdir, exist_ok=True)
     os.chdir(langdir)
     _urls = wmt_data.monolingual[lang]
-    parallelized_commandline('wget', _urls, max_processes=maxp)
+    parallelized_download('wget', _urls, max_processes=maxp)
     os.chdir('../../..')
 
 
-def download_wmt_parallel(corpus_name, maxp=5):    
+def download_wmt_parallel(corpus_name):    
     corpusdir = 'data/parallel/' + corpus_name + '/'
     os.makedirs(corpusdir, exist_ok=True)
     os.chdir(corpusdir)
     url = wmt_data.parallel[corpus_name]
-    parallelized_commandline('wget', [url])
+    parallelized_download('wget', [url])
     os.chdir('../../..')
 
-for corpus_name in wmt_data.parallel:    
-    download_wmt_parallel(corpus_name)
-    break
 
-'''
-http://www.statmt.org/wmt13/training-parallel-europarl-v7.tgz
-http://www.statmt.org/wmt15/europarl-v8.fi-en.tgz
-http://www.statmt.org/wmt13/training-parallel-commoncrawl.tgz
-http://www.statmt.org/wmt13/training-parallel-un.tgz
-http://www.statmt.org/wmt15/training-parallel-nc-v10.tgz
-http://www.statmt.org/wmt10/training-giga-fren.tar
-http://www.statmt.org/wmt15/wiki-titles.tgz
-'''
+def get_all_wmt_monolingual():
+    for lang in wmt_data.monolingual:
+        download_wmt_monolingual(lang)
+    
+def get_all_wmt_parallel():
+    for corpus_name in wmt_data.parallel:    
+        download_wmt_parallel(corpus_name)
 
-
-
+def get_all_wmtdata():
+    thread_mono = Process(target = get_all_wmt_monolingual).start()
+    thread_para = Process(target = get_all_wmt_parallel).start()
+    
+if __name__ == '__main__':
+    get_all_wmtdata()
--- utils.py
+++ utils.py
@@ -3,17 +3,10 @@
 import subprocess
 import os
 
-def parallelized_commandline(command, files, arguments="", max_processes=2):
+def parallelized_download(command, files, arguments="", max_processes=2):
     processes = set()
     for name in files:
         processes.add(subprocess.call([command, name, arguments],
                                        stdout=open(os.devnull, 'wb')))
         if len(processes) >= max_processes:
             os.wait()
-            processes.difference_update(
-                [p for p in processes if p.poll() is not None])
-            
-    #Check if all the child processes were closed
-    for p in processes:
-        if p.poll() is None:
-            p.wait()
