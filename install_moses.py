6da69a71772c882c4896457d5620921906eb9a46
alvations <alvations@gmail.com>
2015-08-03 14:51:01 +0200

    added post-installation checks

--- install_moses.py
+++ install_moses.py
@@ -8,7 +8,7 @@ from multiprocessing import Process
 moses_install_url = 'http://www.statmt.org/moses/?n=Development.GetStarted'
 moses_github_repo = 'https://github.com/moses-smt/mosesdecoder.git'    
 moses_training_tools = 'http://www.statmt.org/moses/RELEASE-3.0/binaries/linux-64bit/training-tools/'
-
+moses_sample_model = 'http://www.statmt.org/moses/download/sample-models.tgz'
 ##############################################################################
 
 def is_linux(distro, architecture):
@@ -27,31 +27,37 @@ def run_command(cmd):
     proc = subprocess.Popen(cmd, shell=True, stdin=None, 
                             #stdout=open(os.devnull,"wb"), 
                             stderr=subprocess.STDOUT, executable="/bin/bash")
+    proc.wait()
     return proc
 
 
 def download_moses_training_tools():
     if os.path.exists('moses-training-tools'): return;
-    proc = run_command('wget -r --no-parent --reject "index.html*" %s' % 
-                       moses_training_tools)
-    proc.wait()
+    run_command('wget -r --no-parent --reject "index.html*" %s' % 
+                moses_training_tools)
     path_to_training_tools = str('www.statmt.org/moses/RELEASE-3.0/binaries/'
                                 'linux-64bit/training-tools/')
-    proc = run_command('mv '+ path_to_training_tools + ' moses-training-tools')
-    proc.wait()
-    proc = run_command('rm -rf www.statmt.org/')
+    run_command('mv '+ path_to_training_tools + ' moses-training-tools')
+    run_command('rm -rf www.statmt.org/')
     
 def download_moses_github_repo():
     if os.path.exists('mosesdecoder'): return;
-    proc = run_command('git clone '+moses_github_repo)
-    proc.wait()
+    run_command('git clone '+moses_github_repo)
     
     
 def install_moses():
     os.chdir('mosesdecoder/')
     if os.path.exists('bin/moses'): return;
-    proc = run_command('./bjam -j4 -max-kenlm-order=20')
-    proc.wait()
+    run_command('./bjam -j4 -max-kenlm-order=20')
+
+def check_installed_moses():
+    os.chdir(os.path.expanduser("~")+ '/mosesdecoder')
+    run_command('wget '+ moses_sample_model)
+    proc.wait('tar xzf sample-models.tgz')
+    os.chdir('sample-models')
+    proc = run_command(str('~/mosesdecoder/bin/moses -f '
+                           'phrase-model/moses.ini < phrase-model/in > out'))
+    print(proc.stdout())
     
 ##############################################################################
 
@@ -73,3 +79,7 @@ repo_thread.join(); tool_thread.join()
 # Install moses
 print('Installing Moses...')
 install_moses()
+
+# Check installed moses.
+print('Checking installed Moses...')
+check_installed_moses()
